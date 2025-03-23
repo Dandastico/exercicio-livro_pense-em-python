8d8fdab624a53d2175d96ed836c74a656a865dc1
Dandastico <dandastico.bsb@gmail.com>
2025-03-23 15:42:52 -0300

    resolução do ex Wordle

--- cap08_recursos/wordle.py
+++ cap08_recursos/wordle.py
@@ -1,28 +1,42 @@
 def main():
     tries = 6
     target = "afeto"
-    
+    print("---------- WORDLE ----------")
+    print("Regras:\n1- Você precisa acertar a palavra em até 6 tentativas")
+    print("2- A palavra-alvo tem 5 letras, seu chute também deve ter 5 letras")
+    print("3- Se um 'V' aparecer embaixo da letra, ela está correta na posição correta")
+    print("4- 'A' significa que a letra está correta, mas na posição errada")
     for i in range(tries):
-        guess = input(f"{i+1}ª tentativa: ")
-        if wordle(target, guess):
-            print(f"Você acertou na {i}ª tentativa!")
+        guess = input(f"{i+1}ª tentativa: ").lower()
+        if wordle(guess, target):
+            print(f"Você acertou na {i+1}ª tentativa!")
             break
-        else:
-            pass
+        
 
 
-def wordle(target, guess):
+def wordle(guess, target):
     """
     Função que verifica se  a plavavra adivinhada é correta
     Se não for certo, verifica se os caracteres estão corretos
     Se o caractere for correto, verifica se está na posição certa
     """
+    print(guess)
+    for i in range(len(guess)):
+        # caractere correta e mas na posição errada
+        if guess[i] in target and guess[i] != target[i]:
+            print("A", end='')
+        # caractere está correta na posição correta
+        elif guess[i] in target and guess[i] == target[i]:
+            print("V", end='')
+        # caractere errado
+        else:
+            print(' ', end='')
+    print()
     # usuário acertou a palavra
     if target == guess:
         return True
     
-
-
+    return False
 
 
 if __name__ == "__main__":
